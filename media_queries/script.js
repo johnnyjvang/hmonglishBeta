2209aad76fe988f0d38ea9982b435f41a6005112
jjvang <johnnyjvang@gmail.com>
2021-05-12 18:26:42 -0700

    v1 complete

--- media_queries/script.js
+++ media_queries/script.js
@@ -1,26 +1,169 @@
 const menuIcon = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector('.navbar2');
 
-
 menuIcon.addEventListener("click", () =>{
   navbar.classList.toggle('change')
 });
 
+// colors
+let colors_png = ["black.png", "blue.png", "brown.png", "gray.png", "green.png", "orange.png", "purple.png", "red.png", "yellow.png"];
 
-// document.querySelectorAll('.category-container')[0].addEventListener("click", function() {
-//   // alert('yeehoo');
+let colors = ["black", "blue", "green", "purple", "red", "yellow", "colors"];
+// var colors = ["black", "blue", "brown", "gray", "green", "orange", "purple", "red", "yellow", "colors"];
+
+let colors_hmong = ["black", "blue", "brown", "gray", "green", "orange", "purple", "red", "yellow", "colors"];
+let animals = ["bear", "bull", "cat", "chicken", "cow", "deer", "duck", "elephant", "fox", "goat", "hen", "horse", "lion", "monkey", "tiger", "animals"];
+
+
+// var cardElements = document.getElementsByClassName("category-container");
+// // console.log(elements)
+// for (var i = 0; i < cardElements.length; i++) {
+//   cardElements[i].addEventListener('click', myFunction);
+// }
+//
+// function myFunction() {
 //   var tom1 = new Audio("elephant.mp3");
 //   tom1.play();
-// });
+// }
+let counter = 1
 
-
-var cardElements = document.getElementsByClassName("category-container");
-// console.log(elements)
-for (var i = 0; i < cardElements.length; i++) {
-  cardElements[i].addEventListener('click', myFunction);
+create_button_generator1(colors)
+create_button_generator1(animals)
+if (counter ==1){
+  create_sound(colors)
+  counter = 9999;
 }
 
-function myFunction() {
-  var tom1 = new Audio("elephant.mp3");
-  tom1.play();
+// This makes button generate card's given the category list with according sound mp3
+function create_button_generator1(category_list){
+  let category_name = category_list[category_list.length -1]
+  document.querySelector("." + category_name).addEventListener("click", function() {
+
+    const swiper_str = create_str2(category_list);
+
+    if (document.querySelector(".swiper-container")) {
+      console.log('category already exist');
+      remove_category();
+      document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
+    } else {
+      console.log('category does not exist')
+      document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
+    }
+    create_sound(category_list)
+    var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical',
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      // scrollbar: {
+      //    el: '.swiper-scrollbar',
+      //    draggable: true,
+      // },
+    });
+  });
 }
+
+function remove_category() {
+  let myobj = document.querySelector(".swiper-container");
+  myobj.remove();
+}
+
+function create_sound(category_list) {
+  for (let i = 0; i < category_list.length - 1; i++) {
+
+    let sound_category = category_list[category_list.length - 1];
+    let sound_name = category_list[i];
+    let audio_link = "sounds/" + sound_category + "/" + sound_name + ".mp3";
+
+    console.log(sound_name)
+    console.log(String(i) + ":" + audio_link);
+    console.log(document.querySelectorAll('.sound_element')[i]);
+    document.querySelectorAll('.sound_element')[i].addEventListener("click", function() {
+      console.log("added click function to" + String(i));
+      let voice_recording = new Audio(audio_link);
+       console.log(audio_link)
+      voice_recording.play();
+    });
+  }
+}
+
+
+function create_str1(category_list) {
+
+  const category_string1 = '<div class="swiper-container z-container swiper-container-initialized swiper-container-vertical"> <div class="swiper-wrapper">';
+  // const category_string1 = '<div class="swiper-container z-container"> <div class="swiper-wrapper">';
+  let running_str = "";
+  let running_pagination = "";
+
+  for (let i = 0; i < category_list.length - 1; i++) {
+    const swiper_slide = '  <div class="swiper-slide sound_element">';
+    const category_container = '<div class="category-container">';
+    const image_container = '<div class="image-container">';
+    const image_url = "images/" + category_list[category_list.length - 1] + "/" + category_list[i] + ".png";
+    const image_html = '<img class="image-size" src="' + image_url + '">';
+    // console.log(image_html);
+    const first_half = swiper_slide + category_container + image_container  + image_html;
+    const text_contaer = '<div class="text-container">';
+    const english_text = category_list[i];
+    const hmong_text = category_list[i];
+    const hmong_html = '<p>' + hmong_text + '</p>';
+    const english_html = '<p>' + english_text + '</p>';
+    const row_end = '</div></div></div></div>';
+    const second_half = text_contaer + hmong_html + english_html + row_end;
+    let all_str = first_half + second_half
+    running_str = running_str.concat(all_str);
+    if (i > 0) {
+      let pagination = '<span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide' + String(i+1) +'"></span>'
+      running_pagination = running_pagination.concat(pagination)
+    }
+    }
+    const pag_1 = '<div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets">';
+    const pag_2 = '<span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 1"></span>';
+    const pag_end = '</div><span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>';
+    const end_str = pag_1 + pag_2 + running_pagination + pag_end;
+
+    // const end_str = '</div><div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"></div></div>';
+    let final_str = category_string1 + running_str + end_str;
+    return final_str
+  }
+
+
+  function create_str2(category_list) {
+
+    const category_string1 = '<div class="swiper-container z-container"> <div class="swiper-wrapper">';
+    // const category_string1 = '<div class="swiper-container z-container"> <div class="swiper-wrapper">';
+    let running_str = "";
+    let running_pagination = "";
+
+    for (let i = 0; i < category_list.length - 1; i++) {
+      const swiper_slide = '  <div class="swiper-slide sound_element">';
+      const category_container = '<div class="category-container">';
+      const image_container = '<div class="image-container">';
+      const image_url = "images/" + category_list[category_list.length - 1] + "/" + category_list[i] + ".png";
+      const image_html = '<img class="image-size" src="' + image_url + '">';
+      // console.log(image_html);
+      const first_half = swiper_slide + category_container + image_container  + image_html;
+      const text_contaer = '<div class="text-container">';
+      const english_text = category_list[i];
+      const hmong_text = category_list[i];
+      const hmong_html = '<p>' + hmong_text + '</p>';
+      const english_html = '<p>' + english_text + '</p>';
+      const row_end = '</div></div></div></div>';
+      const second_half = text_contaer + hmong_html + english_html + row_end;
+      let all_str = first_half + second_half
+      running_str = running_str.concat(all_str);
+      if (i > 0) {
+        let pagination = '<span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide' + String(i+1) +'"></span>'
+        running_pagination = running_pagination.concat(pagination)
+      }
+      }
+      const pag_1 = '<div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets">';
+      const pag_2 = '<span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 1"></span>';
+      const pag_end = '</div><span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>';
+      // const end_str = pag_1 + pag_2 + running_pagination + pag_end;
+
+      const end_str = '</div><div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"></div></div>';
+      let final_str = category_string1 + running_str + end_str;
+      return final_str
+    }
