7ae3795a19a960eed28750e2ac22fafa867f7393
jjvang <johnnyjvang@gmail.com>
2021-05-18 12:27:38 -0700

    v2 full 3 views
    changing views from 1-6 works, no hmong white/green toogle yet

--- final_outline/demo_v2/script.js
+++ final_outline/demo_v2/script.js
@@ -26,62 +26,197 @@ let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', '
 //   var tom1 = new Audio("elephant.mp3");
 //   tom1.play();
 // }
-let counter = 1
+let counter = 1;
+let view_number = document.querySelector('.nav-link.active').innerText;
+let active_category = document.querySelector('.categories.active').innerText;
 
-create_button_generator3(colors)
-create_button_generator3_views(animals)
-// create_button_generator6_views(animals)
-create_button_generator6_views(numbers)
-// if (counter ==1){
-//   create_sound(colors)
-//   counter = 9999;
-// }
+
+// initalize buttons when start, runs only once
+if (view_number == 1){
+  create_button_generator3(colors);
+  create_button_generator3(animals);
+  create_button_generator3(numbers);
+  // console.log('intialize 1');
+} else if (view_number == 3) {
+  create_button_generator3_views(colors);
+  create_button_generator3_views(animals);
+  create_button_generator3_views(numbers);
+  // console.log('intialize 3');
+} else {
+  create_button_generator6_views(colors);
+  create_button_generator6_views(animals);
+  create_button_generator6_views(numbers);
+  // console.log('intialize 6');
+}
+
+
+$(".side2 h1").on("click", function() {
+  $(".side2 h1").removeClass("active");
+  $(this).addClass("active");
+  let active_category = document.querySelector('.categories.active').innerText;
+  // console.log(active_category);
+  // console.log(active_category.toLowerCase());
+  // console.log(typeof(active_category));
+  // if (active_category.toLowerCase() == 'colors'){
+  //   console.log('active = colors');
+  // }
+  // else if (active_category.toLowerCase() == 'numbers'){
+  //   console.log('active = numbers');
+  // } else if (active_category.toLowerCase() == 'animals'){
+  //   console.log('active = animals');
+  // }
+})
+
+$(".nav-item a").on("click", function() {
+  $(".nav-item a").removeClass("active");
+  $(this).addClass("active");
+  // if (active_category==null) {
+  //   $(".side2 h1").removeClass("active");
+  //   $(this).addClass("active");
+  // }
+  // console.log(this);
+  let view_number = document.querySelector('.nav-link.active').innerText;
+  let active_category = document.querySelector('.categories.active').innerText;
+  console.log(active_category, 'animal category')
+    // create_remove_view1(active_category);
+    // create_remove_view3(active_category);
+    // create_remove_view6(active_category);
+    // console.log('create new view');
+
+  // console.log(view_number, active_category);
+  if (view_number == 1){
+    if (active_category.toLowerCase() == "colors"){
+      create_remove_view1(colors);
+      console.log('create new colors');
+    } else if (active_category.toLowerCase() == "animals"){
+      create_remove_view1(animals);
+      console.log('create new animals');
+    } else if (active_category.toLowerCase() == "numbers"){
+      create_remove_view1(numbers);
+      console.log('create new numbers');
+    }
+    create_button_generator3(colors);
+    create_button_generator3(animals);
+    create_button_generator3(numbers);
+    console.log('new 1');
+  } else if (view_number == 3) {
+    if (active_category.toLowerCase() == "colors"){
+      create_remove_view3(colors);
+      console.log('create new colors');
+    } else if (active_category.toLowerCase() == "animals"){
+      create_remove_view3(animals);
+      console.log('create new animals');
+    } else if (active_category.toLowerCase() == "numbers"){
+      create_remove_view3(numbers);
+      console.log('create new numbers');
+    }
+    create_button_generator3_views(colors);
+    create_button_generator3_views(animals);
+    create_button_generator3_views(numbers);
+    console.log('new 3');
+  } else {
+    if (active_category.toLowerCase() == "colors"){
+      create_remove_view6(colors);
+      console.log('create new colors');
+    } else if (active_category.toLowerCase() == "animals"){
+      create_remove_view6(animals);
+      console.log('create new animals');
+    } else if (active_category.toLowerCase() == "numbers"){
+      create_remove_view6(numbers);
+      console.log('create new numbers');
+    }
+    create_button_generator6_views(colors);
+    create_button_generator6_views(animals);
+    create_button_generator6_views(numbers);
+    console.log('new 6');
+  }
+});
+
+
+// create_button_generator3(colors)
+// create_button_generator3_views(animals)
+// create_button_generator6_views(numbers)
+
 
 // This makes button generate card's given the category list with according sound mp3
 function create_button_generator1(category_list){
   let category_name = category_list[category_list.length -1]
   document.querySelector("." + category_name).addEventListener("click", function() {
 
-    const swiper_str = create_str2(category_list);
+    let swiper_str = create_str2(category_list);
 
     if (document.querySelector(".swiper-container")) {
-      console.log('category already exist');
+      // console.log('category already exist');
       remove_category();
       document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
     } else {
-      console.log('category does not exist')
+      // console.log('category does not exist')
       document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
     }
     create_sound(category_list)
-    var swiper = new Swiper('.swiper-container', {
+    let swiper = new Swiper('.swiper-container', {
       direction: 'vertical',
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
-      // scrollbar: {
-      //    el: '.swiper-scrollbar',
-      //    draggable: true,
-      // },
     });
   });
 }
 
+
 function create_button_generator3(category_list){
   let category_name = category_list[category_list.length -1]
   document.querySelector("." + category_name).addEventListener("click", function() {
+  create_remove_view1(category_list)
+  });
+}
 
-    const swiper_str = create_str3(category_list);
+function create_remove_view1(category_list){
+
+    let swiper_str = create_str3(category_list);
+    console.log(active_category, "what is active_category???");
+    // if (active_category.toLowerCase() == "colors"){
+    //   console.log(colors);
+    //   let swiper_str = create_str3(colors);
+    //   console.log('new colors');
+    // } else if (active_category.toLowerCase() == "animals"){
+    //   console.log(animals);
+    //   let swiper_str = create_str3(animals);
+    //   console.log('new animals');
+    // } else if (active_category.toLowerCase() == "numbers"){
+    //     console.log(numbers);
+    //   let swiper_str = create_str3(numbers);
+    //   console.log('new numbers');
+    // }
+
+
+
+    // let swiper_str = create_str3(category_list);
 
     if (document.querySelector(".swiper-container")) {
-      console.log('category already exist');
+      // console.log('category already exist');
       remove_category();
       document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
     } else {
-      console.log('category does not exist')
+      // console.log('category does not exist')
       document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
     }
+
     create_sound(category_list)
+
+    // if (active_category.toLowerCase() == "colors"){
+    //   create_sound(colors)
+    //   console.log('new colors sound');
+    // } else if (active_category.toLowerCase() == "animals"){
+    //   create_sound(animals)
+    //   console.log('new animals sound');
+    // } else if (active_category.toLowerCase() == "numbers"){
+    //   create_sound(numbers)
+    //   console.log('new numbers sound');
+    // }
+
+    // create_sound(category_list)
     let swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
       pagination: {
@@ -89,59 +224,64 @@ function create_button_generator3(category_list){
         clickable: true,
       },
     });
-  });
 }
 
 function create_button_generator3_views(category_list){
   let category_name = category_list[category_list.length -1]
   document.querySelector("." + category_name).addEventListener("click", function() {
+  create_remove_view3(category_list)
+  });
+}
 
-    const swiper_str = create_str3(category_list);
+function create_remove_view3(category_list){
+  let swiper_str = create_str3(category_list);
 
-    if (document.querySelector(".swiper-container")) {
-      console.log('category already exist');
-      remove_category();
-      document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
-    } else {
-      console.log('category does not exist')
-      document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
-    }
-    create_sound(category_list)
-    let swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+  if (document.querySelector(".swiper-container")) {
+    // console.log('category already exist');
+    remove_category();
+    document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
+  } else {
+    // console.log('category does not exist')
+    document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
+  }
+  create_sound(category_list)
+  let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
 }
 
 function create_button_generator6_views(category_list){
   let category_name = category_list[category_list.length -1]
   document.querySelector("." + category_name).addEventListener("click", function() {
+  create_remove_view6(category_list)
+  });
+}
 
-    const swiper_str = create_str6(category_list);
+function create_remove_view6(category_list){
+  let swiper_str = create_str6(category_list);
 
-    if (document.querySelector(".swiper-container")) {
-      console.log('category already exist');
-      remove_category();
-      document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
-    } else {
-      console.log('category does not exist')
-      document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
-    }
-    create_sound(category_list)
-    let swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      slidesPerColumn: 2,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+  if (document.querySelector(".swiper-container")) {
+    // console.log('category already exist');
+    remove_category();
+    document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
+  } else {
+    // console.log('category does not exist')
+    document.querySelector(".row-custom").insertAdjacentHTML('afterend', swiper_str);
+  }
+  create_sound(category_list)
+  let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    slidesPerColumn: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
 }
 
@@ -157,14 +297,14 @@ function create_sound(category_list) {
     let sound_category = category_list[category_list.length - 1];
     let sound_name = category_list[i];
     let audio_link = "sounds/" + sound_category + "/" + sound_name + ".mp3";
-
-    console.log(sound_name)
-    console.log(String(i) + ":" + audio_link);
-    console.log(document.querySelectorAll('.sound_element')[i]);
+    //
+    // console.log(sound_name)
+    // console.log(String(i) + ":" + audio_link);
+    // console.log(document.querySelectorAll('.sound_element')[i]);
     document.querySelectorAll('.sound_element')[i].addEventListener("click", function() {
-      console.log("added click function to" + String(i));
+      // console.log("added click function to" + String(i));
       let voice_recording = new Audio(audio_link);
-       console.log(audio_link)
+       // console.log(audio_link)
       voice_recording.play();
     });
   }
